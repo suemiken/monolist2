116bfb228f4ba5973137d8c98bb77f4bcbec1363
suemitu <suemiken@icloud.com>
2016-05-19 12:56:54 +0000

    description destroy br

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,7 +37,7 @@ class User < ActiveRecord::Base
   end
 
   def have(item)
-    haves.create(item_id: item.id)
+    haves.find_or_create_by(item_id: item.id)
   end
 
   def unhave(item)
@@ -49,7 +49,7 @@ class User < ActiveRecord::Base
   end
 
   def want(item)
-    wants.create(item_id: item.id)
+    wants.find_or_create_by(item_id: item.id)
   end
   
   def unwant(item)
